7cba8ff231d385f22833bb8600b1c09c5f1162ab
Edmund Aryee <lyrixgh@yahoo.com>
2025-02-18 23:09:42 +0000

    UserAndUserRoleTableChangesMigrated

--- backend/PrestigePathway/PrestigePathway.DataAccessLayer/Migrations/20250218230237_AuthServiceRefactored.Designer.cs
+++ backend/PrestigePathway/PrestigePathway.DataAccessLayer/Migrations/20250218230237_AuthServiceRefactored.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrestigePathway.DataAccessLayer;
 
@@ -11,9 +12,11 @@ using PrestigePathway.DataAccessLayer;
 namespace PrestigePathway.DataAccessLayer.Migrations
 {
     [DbContext(typeof(SocialServicesDbContext))]
-    partial class SocialServicesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250218230237_AuthServiceRefactored")]
+    partial class AuthServiceRefactored
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/PrestigePathway/PrestigePathway.DataAccessLayer/Migrations/20250218230237_AuthServiceRefactored.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrestigePathway.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthServiceRefactored : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_RoleID",
+                table: "UserRoles");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOnUtc",
+                table: "Roles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOnUtc",
+                table: "Roles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserID",
+                table: "UserRoles",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserID",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOnUtc",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOnUtc",
+                table: "Roles");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_RoleID",
+                table: "UserRoles",
+                column: "RoleID",
+                principalTable: "Users",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
