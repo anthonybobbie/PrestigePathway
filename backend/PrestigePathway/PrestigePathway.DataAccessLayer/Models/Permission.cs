429200cb6db77582614a0fc989fa2cb55fb1ca07
anthony bobbie <anthony.bobbie@hotmail.com>
2025-03-05 12:52:52 -0500

    updated

---
+++ backend/PrestigePathway/PrestigePathway.DataAccessLayer/Models/Permission.cs
@@ -0,0 +1,12 @@
+﻿using PrestigePathway.DataAccessLayer.Abstractions;
+using PrestigePathway.DataAccessLayer.Repositories;
+
+namespace PrestigePathway.DataAccessLayer.Models;
+
+public class Permission : IEntity, IEntityTracker
+{
+    public int ID { get; set; }
+    public string Name { get; set; } // eg Read:Booking
+    public DateTime? ModifiedOnUtc { get; set; }
+    public DateTime CreatedOnUtc { get; set; }
+}
